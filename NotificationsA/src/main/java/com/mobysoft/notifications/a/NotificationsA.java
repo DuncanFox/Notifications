fc0cd19a5c3068c5836e1f182e4185fd88d0cc9d
Duncan <duncan.sanders-fox@mobysoft.com>
2016-03-17 14:15:56 +0000

    Initial Checkin 2

---
+++ NotificationsA/src/main/java/com/mobysoft/notifications/a/NotificationsA.java
@@ -0,0 +1,7 @@
+package com.mobysoft.notifications.a;
+
+/**
+ * Created by Duncan on 17/03/2016.
+ */
+public class NotificationsA {
+}
---
+++ NotificationsB/src/main/java/com/mobysoft/notifications/b/NotificationsB.java
@@ -0,0 +1,7 @@
+package com.mobysoft.notifications.b;
+
+/**
+ * Created by Duncan on 17/03/2016.
+ */
+public class NotificationsB {
+}
